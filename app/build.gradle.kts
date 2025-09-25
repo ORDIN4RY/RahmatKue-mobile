33d379e46942ffa9cde714676c46787de34ce797
Faza <ahmadfaza200@gmail.com>
2025-09-25 09:53:37 +0700

    ganti warna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
