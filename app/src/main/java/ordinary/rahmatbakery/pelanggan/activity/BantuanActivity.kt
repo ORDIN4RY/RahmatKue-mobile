7863c9488da9a5864665c93d26e0e12275179066
Thoriq130 <thoriqkamal13@gmail.com>
2025-11-13 09:47:43 +0700

    Button & pesanan

--- app/src/main/java/ordinary/rahmatbakery/pelanggan/activity/BantuanActivity.kt
+++ app/src/main/java/ordinary/rahmatbakery/pelanggan/activity/BantuanActivity.kt
@@ -3,18 +3,15 @@ package ordinary.rahmatbakery.pelanggan.activity
 import ordinary.rahmatbakery.pelanggan.adapter.HelpAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD:app/src/main/java/ordinary/rahmatbakery/BantuanActivity.kt
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ordinary.rahmatbakery.pelanggan.model.HelpItem
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-=======
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ordinary.rahmatbakery.R
 
->>>>>>> 93b554b1041b7e92df89a63453fda2a5789281b9:app/src/main/java/ordinary/rahmatbakery/pelanggan/activity/BantuanActivity.kt
 class BantuanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ordinary/rahmatbakery/pelanggan/model/Paket.kt
+++ app/src/main/java/ordinary/rahmatbakery/pelanggan/model/Paket.kt
@@ -1,7 +1,10 @@
 package ordinary.rahmatbakery.pelanggan.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
+@Parcelize
 @Serializable
 data class Paket(
     val id : String,
@@ -10,10 +13,12 @@ data class Paket(
     val foto: String,
     val harga: Int,
     val detail: List<Detail> = emptyList()
-)
+) : Parcelable
 
+
+@Parcelize
 @Serializable
 data class Detail(
     val produk: Produk,
     val jumlah: Int
-)
+) : Parcelable
